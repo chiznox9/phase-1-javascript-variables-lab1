35230e33c4a86d11259c5bd3ea1b772b7ac6e76e
james kariuki <muchiznox@gmail.com>
2025-03-16 16:54:34 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file!
+// Code your solution in this file!
+
+// Define companyName as a constant and set it to "Scuber"
+const companyName = "Scuber";
+
+// Define mostProfitableNeighborhood using let and set it to "Chelsea"
+let mostProfitableNeighborhood = "Chelsea";
+
+// Define companyCeo using let and set it to "Susan Smith"
+let companyCeo = "Susan Smith";
